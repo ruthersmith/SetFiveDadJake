28fa4a73fdd4bd41b644dc368876c2179f1f8829
ruthersmit B <bercyruthersmithadvid@gmail.com>
2020-01-12 22:49:07 -0500

    a better solution refacturing better

--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const request = require('request');
-var jokes_with_sqlite = require('../models/jokes_with_sqlite');
 
-
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  //importJokes(term);
-  console.log(req.body);
-  // res.render('index', { title: 'Express' });
-  res.render('front', { title: 'Express' });
+    console.log(req.body);
+    res.render('front', { title: 'Express' });
 });
-
-
-
-router.get('/joke/:id', function(req,res,next){
-  let jokeid  = (req.params.id);
-  res.render('joke', { id: jokeid});
-});
-
-router.post('/joke/getJokeComments',function (req,res,next) {
-    // let sql = `SELECT * FROM comments WHERE jokeid like ?`;
-    // jokes_with_sqlite.get_from_db(sql,[req.body.id],res);
-
-  let sql = `SELECT * FROM comments WHERE jokeid like 'xc21Lmbxcib'`;
-  jokes_with_sqlite.get_from_db(sql,[req.body.id],res);
-});
-
-
-router.post('/joke/submitComment',function (req,res,next) {
-  let sql = "INSERT INTO comments(jokeid,rating,comment) VALUES(?,?,?)";
-  let values = [req.body.jokeid,req.body.rate,req.body.comment];
-  console.log(req.body);
-  jokes_with_sqlite.commit_to_db(sql,values);
-});
-
-
-router.post('/getJokesfromdb', function(req, res, next) {
-  let sql = "SELECT * FROM jokes";
-  jokes_with_sqlite.get_from_db(sql,[],res);
-});
-
 
 module.exports = router;
--- routes/search.js
+++ routes/search.js
@@ -19,4 +19,24 @@ router.get('/:id', function(req,res,next){
     res.render('jokepage', { id: jokeid});
 });
 
+router.post('/getJokeById', function (req,res,next) {
+    let  jokeid = req.body.id;
+    let sql = 'SELECT * FROM jokes WHERE joke_id like ?';
+    jokes_with_sqlite.get_from_db(sql, jokeid,res);
+});
+
+router.post('/postComment', function (req,res,next) {
+    console.log(req.body);
+    let sql = "INSERT INTO comments(jokeid,rating,comment) VALUES(?,?,?)";
+    let param = [req.body.joke_id,req.body.rating,req.body.comment,];
+    jokes_with_sqlite.commit_to_db(sql,param);
+});
+
+router.post('/getComments', function (req,res,next) {
+    let  jokeid = req.body.id;
+    let sql = 'SELECT comment FROM comments WHERE jokeid like ?';
+    jokes_with_sqlite.get_from_db(sql, jokeid,res);
+});
+
+
 module.exports = router;
